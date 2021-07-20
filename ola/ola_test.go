ec09a350ab869a3d06750a0835144d7abc3d8857
Bruno Quindeler <21955104+Brunoquindeler@users.noreply.github.com>
2021-07-20 14:43:43 -0300

    Feature/ola (#1)
    * feat: Add ola.go and ola_test.go
    
    * feat: add const prefixoOlaPortugues
    
    * feat: add subtests t.Run
    
    * feat: Add verificaMensagemCorreta with t.Helper() to help tests verification
    
    * fix: added checks for empty strings parameters on func Ola()
    
    * feat: Added test case with other language
    
    * fit: Refactor test case with other language
    
    * fix: Refining func Ola() with switch case
    
    * fix: Refining consts and create func prefixoDeSaudacao()
    
    * fix: Refinings

---
+++ ola/ola_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestOla(t *testing.T) {
+
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("Resultado: '%s' | Esperado: '%s'", resultado, esperado)
+		}
+	}
+
+	t.Run("Diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Elodie", "")
+		esperado := "Olá, Elodie"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("Diz 'Olá, Mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("", "")
+		esperado := "Olá, Mundo"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "esp")
+		esperado := "Hola, Elodie"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "fr")
+		esperado := "Bonjour, Elodie"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+}
