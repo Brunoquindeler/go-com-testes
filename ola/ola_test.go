8ecab13f6d6919feddbd91c5f341fd255844974f
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-07-20 11:56:27 -0300

    feat: add subtests t.Run

--- ola/ola_test.go
+++ ola/ola_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Bruno")
-	esperado := "Olá, Bruno"
 
-	if resultado != esperado {
-		t.Errorf("Resultado: '%s' | Esperado: '%s'", resultado, esperado)
-	}
+	t.Run("Diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Bruno")
+		esperado := "Olá, Bruno"
+
+		if resultado != esperado {
+			t.Errorf("Resultado: '%s' | Esperado: '%s'", resultado, esperado)
+		}
+	})
+
+	t.Run("Diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		if resultado != esperado {
+			t.Errorf("Resultado: '%s' | Esperado: '%s'", resultado, esperado)
+		}
+	})
 }
