e9b9e0266786883525ff4ad83cf03f18463f667d
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-27 14:26:01 -0300

    feat: addded benchmark to func Repetidor

--- iteracao/repetidor_test.go
+++ iteracao/repetidor_test.go
@@ -10,3 +10,9 @@ func TestRepetidor(t *testing.T) {
 		t.Errorf("\nEsperado: %v\nObtido: %v", esperado, repeticoes)
 	}
 }
+
+func BenchmarkRepetidor(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeditor("a")
+	}
+}
