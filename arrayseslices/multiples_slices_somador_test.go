1451ce07c449da59b3b727076839139e080fbaf8
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 11:35:41 -0300

    fix: modified check to reflect.DeepEqual() to check slices

--- arrayseslices/multiples_slices_somador_test.go
+++ arrayseslices/multiples_slices_somador_test.go
@@ -1,15 +1,18 @@
 package arrayseslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMultiplesSlicesSomador(t *testing.T) {
 	slice1 := []int{1, 2}
 	slice2 := []int{0, 9}
 
-	obtido := MultiplesSlicesSomador(slice1, slice2)
 	esperado := []int{3, 9}
+	obtido := MultiplesSlicesSomador(slice1, slice2)
 
-	if esperado != obtido {
+	if !reflect.DeepEqual(esperado, obtido) {
 		t.Errorf("\nDado: %v %v \nEsperado: %v \nObtido: %v", slice1, slice2, esperado, obtido)
 	}
 }
