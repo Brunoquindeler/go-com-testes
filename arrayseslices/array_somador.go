1be33c3b3f63232bd5345d8e139a93bf45dec280
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 10:28:42 -0300

    docs: added comments, example and benchmark

--- arrayseslices/array_somador.go
+++ arrayseslices/array_somador.go
@@ -1,5 +1,7 @@
 package arrayseslices
 
+// ArraySomador recebe um array de inteiros
+// e retorna a soma total de todos os números contidos nele
 func ArraySomador(numeros [5]int) int {
 	total := 0
 
--- arrayseslices/array_somador_test.go
+++ arrayseslices/array_somador_test.go
@@ -1,6 +1,7 @@
 package arrayseslices
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -12,5 +13,17 @@ func TestArraySomador(t *testing.T) {
 
 	if esperado != obtido {
 		t.Errorf("\nDado: %v \nEsperado: %d \nObtido: %d", numeros, esperado, obtido)
+	}
+}
+
+func ExampleArraySomador() {
+	total := ArraySomador([5]int{1, 2, 3, 4, 5})
+	fmt.Println(total)
+	// Output: 15
+}
+
+func BenchmarkArraySomador(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ArraySomador([5]int{1, 2, 3, 4, 5})
 	}
 }
