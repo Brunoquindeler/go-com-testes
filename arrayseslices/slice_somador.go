f1fe114bab8c3be5119943d7497f852a4a7a22a9
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 11:04:50 -0300

    fix: modified the func SliceSomador() to work correctly

--- arrayseslices/slice_somador.go
+++ arrayseslices/slice_somador.go
@@ -3,5 +3,10 @@ package arrayseslices
 // ArraySomador recebe um slice de inteiros
 // e retorna a soma total de todos os números contidos nele
 func SliceSomador(numeros []int) int {
-	return 0
+	total := 0
+
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
 }
