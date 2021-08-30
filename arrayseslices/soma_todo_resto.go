ff9c1d4e8c05541477a1cf0df55456dd2168b793
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 16:34:26 -0300

    feat: added check for empty slices

--- arrayseslices/soma_todo_resto.go
+++ arrayseslices/soma_todo_resto.go
@@ -4,8 +4,13 @@ func SomaTodoResto(slices ...[]int) []int {
 	var total []int
 
 	for _, slice := range slices {
-		final := slice[1:]
-		total = append(total, SliceSomador(final))
+		if len(slice) == 0 {
+			total = append(total, 0)
+		} else {
+			final := slice[1:]
+			total = append(total, SliceSomador(final))
+		}
+
 	}
 
 	return total
--- arrayseslices/soma_todo_resto_test.go
+++ arrayseslices/soma_todo_resto_test.go
@@ -6,15 +6,31 @@ import (
 )
 
 func TestSomaTodoResto(t *testing.T) {
-	slice1 := []int{1, 2}
-	slice2 := []int{0, 9}
 
-	esperado := []int{2, 9}
+	t.Run("Soma alguns slices", func(t *testing.T) {
+		slice1 := []int{1, 2}
+		slice2 := []int{0, 9}
 
-	obtido := SomaTodoResto(slice1, slice2)
+		esperado := []int{2, 9}
 
-	if !reflect.DeepEqual(esperado, obtido) {
-		t.Errorf("\nDado: %v %v \nEsperado: %v \nObtido: %v", slice1, slice2, esperado, obtido)
-	}
+		obtido := SomaTodoResto(slice1, slice2)
+
+		if !reflect.DeepEqual(esperado, obtido) {
+			t.Errorf("\nDado: %v %v \nEsperado: %v \nObtido: %v", slice1, slice2, esperado, obtido)
+		}
+	})
+
+	t.Run("Soma alguns slices vazios de forma segura", func(t *testing.T) {
+		slice1 := []int{}
+		slice2 := []int{0, 9, 2}
+
+		esperado := []int{0, 11}
+
+		obtido := SomaTodoResto(slice1, slice2)
+
+		if !reflect.DeepEqual(esperado, obtido) {
+			t.Errorf("\nDado: %v %v \nEsperado: %v \nObtido: %v", slice1, slice2, esperado, obtido)
+		}
+	})
 
 }
