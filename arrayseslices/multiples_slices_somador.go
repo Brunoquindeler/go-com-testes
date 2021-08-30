5dadd497e3a4343f119a494ca6e5e984f6750951
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 11:51:40 -0300

    fix: modified func MultiplesSlicesSomador() to work correctly

--- arrayseslices/multiples_slices_somador.go
+++ arrayseslices/multiples_slices_somador.go
@@ -1,5 +1,11 @@
 package arrayseslices
 
 func MultiplesSlicesSomador(slices ...[]int) (total []int) {
+	quantidadesDeSlices := len(slices)
+	total = make([]int, quantidadesDeSlices)
+
+	for i, numeros := range slices {
+		total[i] = SliceSomador(numeros)
+	}
 	return
 }
