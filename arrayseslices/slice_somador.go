8cf8be3b261973d7bd0316ed0fc21a90e95d5c48
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-08-30 11:03:44 -0300

    feat: added minimal func SliceSomador() to run test

---
+++ arrayseslices/slice_somador.go
@@ -0,0 +1,7 @@
+package arrayseslices
+
+// ArraySomador recebe um slice de inteiros
+// e retorna a soma total de todos os números contidos nele
+func SliceSomador(numeros []int) int {
+	return 0
+}
