ddb77def73cf9780384b5ebb86934f9c089ac5fb
Bruno Quindeler <bruno.quindeler@gmail.com>
2021-09-05 01:29:51 -0300

    refac: added names and modified output of the tests

--- estruturasmetodosinterfaces/geometria_test.go
+++ estruturasmetodosinterfaces/geometria_test.go
@@ -15,18 +15,21 @@ func TestPerimetro(t *testing.T) {
 func TestArea(t *testing.T) {
 
 	cases := []struct {
+		nome      string
 		geometria Geometria
 		esperado  float64
 	}{
-		{Retangulo{12, 6}, 72.0},
-		{Circulo{10}, 314.1592653589793},
-		{Triangulo{12, 6}, 36.0},
+		{nome: "Retângulo", geometria: Retangulo{Largura: 12, Altura: 6}, esperado: 72.0},
+		{nome: "Círculo", geometria: Circulo{Raio: 10}, esperado: 314.1592653589793},
+		{nome: "Triângulo", geometria: Triangulo{Base: 12, Altura: 6}, esperado: 36.0},
 	}
 
 	for _, tt := range cases {
-		obtido := tt.geometria.Area()
-		if obtido != tt.esperado {
-			t.Errorf("\nEsperado: %.2f \nObtido: %.2f", tt.esperado, obtido)
-		}
+		t.Run(tt.nome, func(t *testing.T) {
+			obtido := tt.geometria.Area()
+			if obtido != tt.esperado {
+				t.Errorf("\nGeometria: %#v \nEsperado: %.2f \nObtido: %.2f", tt.geometria, tt.esperado, obtido)
+			}
+		})
 	}
 }
